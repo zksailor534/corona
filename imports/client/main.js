ca346c8287a55646c88135b29fd826252f40b5f9
zksailor534 <nate.greene@gmail.com>
2016-09-02 08:30:48 -0400

    Move all client and server code to imports folder

---
+++ imports/client/main.js
@@ -0,0 +1,29 @@
+import { createApp } from 'mantra-core';
+import initContext from './configs/context';
+
+// Redux
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+// Modules
+import coreModule from './modules/core';
+import userModule from './modules/user';
+
+// Import styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Combine Reducers
+const reducer = combineReducers({
+  ...coreModule.reducer,
+  ...userModule.reducer,
+  routing: routerReducer
+});
+
+// Init Context
+const context = initContext({reducer});
+
+// Create App
+const app = createApp(context);
+app.loadModule(coreModule);
+app.loadModule(userModule);
+app.init();

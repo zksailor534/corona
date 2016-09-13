e12fc5bea8d2e1a28e5d54a85738fbf20271587d
zksailor534 <nate.greene@gmail.com>
2016-09-13 13:35:20 -0400

    Functioning login validation

--- imports/client/modules/core/components/login.js
+++ imports/client/modules/core/components/login.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Row, Col, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
-// import validate from 'validate.js';
+import {
+  Row,
+  Col,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Button,
+  HelpBlock,
+} from 'react-bootstrap';
+import validate from 'validate.js';
+
+const constraints = {
+  email: {
+    email: true,
+  },
+};
 
 class Login extends React.Component {
   constructor(props) {
@@ -9,16 +23,28 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      validEmail: false,
+      validPassword: false,
+      emailValidationState: null,
+      passwordValidationState: null,
     };
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.submitLogin({
-      email: this.state.email,
-      password: this.state.password,
-      component: this,
-    });
+    if (this.state.password.length) {
+      this.props.submitLogin({
+        email: this.state.email,
+        password: this.state.password,
+        component: this,
+      });
+    } else {
+      this.setState({
+        validPassword: false,
+        passwordValidationState: null,
+        passwordMessage: 'Please enter password',
+      });
+    }
   }
 
   setValue(event) {
@@ -28,34 +54,84 @@ class Login extends React.Component {
     this.setState(newState);
   }
 
+  validateEmail(event) {
+    event.preventDefault();
+    if (!event.target.value.length) {
+      this.setState({
+        validEmail: false,
+        emailValidationState: null,
+        emailMessage: 'Please enter valid email',
+      });
+    } else {
+      const res = validate({ email: event.target.value }, constraints);
+      if (res !== undefined) {
+        this.setState({
+          validEmail: false,
+          emailValidationState: 'error',
+          emailMessage: res.email[0],
+        });
+      } else {
+        this.setState({
+          validEmail: true,
+          emailValidationState: 'success',
+          emailMessage: null,
+        });
+      }
+    }
+  }
+
   render() {
+    const {
+      email,
+      emailMessage,
+      emailValidationState,
+      password,
+      passwordMessage,
+      passwordValidationState,
+    } = this.state;
+    const buttonState = !this.state.validEmail;
     return (
       <Row>
         <Col xs={ 12 } sm={ 6 } md={ 4 }>
           <h4 className="page-header">Login</h4>
           <form ref="login" className="login" onSubmit={ this.handleSubmit.bind(this) }>
-            <FormGroup>
+            <FormGroup
+              controlId="emailAddress"
+              validationState={emailValidationState}
+            >
               <ControlLabel>Email Address</ControlLabel>
               <FormControl
                 type="email"
                 name="email"
                 placeholder="Email Address"
-                value={this.state.email}
+                value={email}
+                onBlur={this.validateEmail.bind(this)}
                 onChange={this.setValue.bind(this)}
               />
+              {emailMessage ? <HelpBlock>{emailMessage}</HelpBlock> : null }
             </FormGroup>
-            <FormGroup>
+            <FormGroup
+              controlId="password"
+              validationState={passwordValidationState}
+            >
               <ControlLabel>Password</ControlLabel>
               <FormControl
                 type="password"
                 name="password"
                 placeholder="Password"
-                value={this.state.password}
+                value={password}
                 onChange={this.setValue.bind(this)}
               />
-              <Link className="pull-right" to="/recover-password">Forgot Password?</Link>
+              {passwordMessage ? <HelpBlock>{passwordMessage}</HelpBlock> : null }
             </FormGroup>
-            <Button type="submit" bsStyle="success">Login</Button>
+            <Button
+              type="submit"
+              bsStyle="success"
+              disabled={buttonState}
+            >
+              Login
+            </Button>
+            <Link className="pull-right" to="/recover-password">Forgot Password?</Link>
           </form>
         </Col>
       </Row>

3b54d007559db778d7a1cc89e4a3782f83e9e5b1
zksailor534 <nate.greene@gmail.com>
2016-09-15 14:54:59 -0400

    Fully functioning recover-password form

--- imports/client/modules/core/components/recover-password.js
+++ imports/client/modules/core/components/recover-password.js
@@ -1,59 +1,30 @@
 import React from 'react';
-import { Row, Col, Alert, FormGroup, FormControl, Button } from 'react-bootstrap';
-// import validate from 'validate.js';
+import { Form, Col, Alert, Button } from 'react-bootstrap';
+import { Field } from 'redux-form';
+import { inputField } from './form-fields';
 
-class RecoverPassword extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-    };
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.recoverPassword({ email: this.state.email });
-  }
-
-  setValue(event) {
-    event.preventDefault();
-    const newState = {};
-    newState[event.target.name] = event.target.value;
-    this.setState(newState);
-  }
-
-  render() {
-    return (
-      <Row>
-        <Col xs={ 12 } sm={ 6 } md={ 4 }>
-          <h4 className="page-header">Recover Password</h4>
-          <Alert bsStyle="info">
-            Enter your email address below to receive a link to reset your password.
-          </Alert>
-          <form
-            ref="recoverPassword"
-            className="recover-password"
-            onSubmit={ this.handleSubmit.bind(this) }
-          >
-            <FormGroup>
-              <FormControl
-                type="email"
-                name="email"
-                placeholder="Email Address"
-                value={this.state.email}
-                onChange={this.setValue.bind(this)}
-              />
-            </FormGroup>
-            <Button type="submit" bsStyle="success">Recover Password</Button>
-          </form>
-        </Col>
-      </Row>
-    );
-  }
-}
+const RecoverPassword = (props) => {
+  const { handleSubmit, pristine, submitting, invalid } = props;
+  return (
+    <Col xs={ 12 } sm={ 8 } md={ 6 } lg={ 6 }>
+      <h4 className='page-header'>Recover Password</h4>
+      <Alert bsStyle='info'>
+        Enter your email address below to receive a link to reset your password.
+      </Alert>
+      <Form onSubmit={handleSubmit}>
+        <Field name='email' type='email' component={inputField} label='Email Address'/>
+        <Button type='submit' disabled={pristine || submitting || invalid}>Submit</Button>
+      </Form>
+    </Col>
+  );
+};
 
 RecoverPassword.propTypes = {
-  recoverPassword: React.PropTypes.func.isRequired,
+  handleSubmit: React.PropTypes.func,
+  pristine: React.PropTypes.bool,
+  reset: React.PropTypes.func,
+  submitting: React.PropTypes.bool,
+  invalid: React.PropTypes.bool,
 };
 
 export default RecoverPassword;
